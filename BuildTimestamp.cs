7ac61a04a40b7ad34dda301591501cda9d0ef602
Chris Dailey <nitz@users.noreply.github.com>
2022-01-27 12:21:19 -0500

    Fixed errors & added comments for generated source.

--- BuildTimestamp.cs
+++ BuildTimestamp.cs
@@ -17,16 +17,54 @@ using System;
 
 namespace {nameof(cmdwtf)}
 {{
-	internal static partial class {nameof(BuildTimestamp)}
+
+	/// <summary>
+	/// A static class representing when it was generated.
+	/// </summary>
+	internal static class {nameof(BuildTimestamp)}
 	{{
+		/// <summary>
+		/// The timestamp (in Windows FILETIME, the number of 100ns intervals since
+		/// 12:00 midnight, January 1, 1601 C.E UTC) from when this source was generated.
+		/// </summary>
 		public const long FileTime = {executeTime.ToFileTime()};
+
+
+		/// <summary>
+		/// The timestamp (in ticks elapsed since the beginning of the 21st century)
+		/// from when this source was generated.
+		/// </summary>
 		public const long Ticks = {executeTime.Ticks};
+
+		/// <summary>
+		/// The time this source was generated, as a <see cref=""DateTimeKind.Utc""/> <see cref=""DateTime""/>.
+		/// </summary>
 		public static DateTime BuildTimeUtc {{ get; }} = DateTime.FromFileTimeUtc(FileTime);
-		public static DateTimeOffset BuildTime {{ get; }} = new DateTimeOffset(BuildTime);
+
+		/// <summary>
+		/// The time this source was generated, as a <see cref=""DateTimeOffset""/>.
+		/// </summary>
+		public static DateTimeOffset BuildTime {{ get; }} = new DateTimeOffset(BuildTimeUtc);
+
+		/// <summary>
+		/// The time this source was generated as a Unix timestamp in local time.
+		/// </summary>
 		public static long UnixTime {{ get; }} = BuildTime.ToUnixTimeSeconds();
-		public static long UnixTimeUtc {{ get; }} = BuildTime.Now.ToUniversalTime().ToUnixTimeSeconds();
+
+		/// <summary>
+		/// The time this source was generated as a Unix timestamp in UTC.
+		/// </summary>
+		public static long UnixTimeUtc {{ get; }} = BuildTime.ToUniversalTime().ToUnixTimeSeconds();
+
+		/// <summary>
+		/// The time this source was generated as a Unix millisecond timestamp in local time.
+		/// </summary>
 		public static long UnixTimeMilliseconds {{ get; }} = BuildTime.ToUnixTimeMilliseconds();
-		public static long UnixTimeMillisecondsUtc {{ get; }} = BuildTime.Now.ToUniversalTime().ToUnixTimeMilliseconds();
+
+		/// <summary>
+		/// The time this source was generated as a Unix millisecond timestamp in UTC.
+		/// </summary>
+		public static long UnixTimeMillisecondsUtc {{ get; }} = BuildTime.ToUniversalTime().ToUnixTimeMilliseconds();
 	}}
 }}
 ";
